b90754576e2dae4c85eac5d2a1e546be90a88805
elfetetotaj <elfetetotaj@hotmail.com>
2021-06-01 19:23:27 +0200

    Starting with React

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ducks } from './demo';
-import DuckItem from './DuckItem';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setCities(response.data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-          <DuckItem duck={duck} />
-        ))}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='Cities' />
+
+        <List>
+        {cities.map((city: any) => (
+            <List.Item key={city.id}>
+              {city.CityName}
+              {city.ZipCode}
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }
