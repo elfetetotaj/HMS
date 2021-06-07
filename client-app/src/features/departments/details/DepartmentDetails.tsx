59e1a6b2f7c53f9b442186a88dfe828d12f71d58
Xhastin Bojaxhiu <xhastinbojaxhiu@outlook.com>
2021-06-07 22:08:38 +0200

    Section 9 permirsim

--- client-app/src/features/departments/details/DepartmentDetails.tsx
+++ client-app/src/features/departments/details/DepartmentDetails.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
