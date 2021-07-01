07a0184e7012abc09e213fbe4d13520b0e5abb4e
Erza Shatri <es47732@ubt-uni.net>
2021-07-01 22:59:48 +0200

    Patient and Doctor edit fixed

--- client-app/src/features/doctors/details/DoctorDetailedInfo.tsx
+++ client-app/src/features/doctors/details/DoctorDetailedInfo.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
 import React from 'react'
 import {Segment, Grid, Icon} from 'semantic-ui-react'
@@ -27,7 +28,7 @@ export default observer(function DoctorDetailedInfo({doctor}: Props) {
                     </Grid.Column>
                     <Grid.Column width={15}>
             <span>
-              {doctor.dateofbirth}
+            {format(doctor.dateofbirth!, 'dd MMM yyyy h:mm aa')}
             </span>
                     </Grid.Column>
                 </Grid>
